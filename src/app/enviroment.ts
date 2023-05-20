a25c3228d06e3d639a22377b39114767189aeae3
Do1902 <duyhvhv123@gmail.com>
2023-05-20 21:37:53 +0700

    upl

--- src/app/enviroment.ts
+++ src/app/enviroment.ts
@@ -1,5 +1,7 @@
 export const environment = {
   // apiUrl: 'http://127.0.0.1:8000/api',
-  apiUrl: 'http://hanashop.crysys.net/public/api',
+  // apiUrl: 'http://hanashop.crysys.net/public/api',
+  apiUrl: 'https://hanashop.crysys.net/public/api',
+
   apiKey: 'your_api_key_here',
 };
